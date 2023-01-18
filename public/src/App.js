012729a32b7fda9307aa0991dd4dc236e006fb64
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2023-01-18 14:34:36 +0700

    optimize

--- public/src/App.js
+++ public/src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Chat from "./pages/Chat";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import SetAvatar from "./pages/SetAvatar";
+
+const Chat = lazy(() => import("./pages/Chat"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const SetAvatar = lazy(() => import("./pages/SetAvatar"));
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/setAvatar" element={<SetAvatar />} />
-        <Route path="/" element={<Chat />} />
-      </Routes>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/setAvatar" element={<SetAvatar />} />
+          <Route path="/" element={<Chat />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
